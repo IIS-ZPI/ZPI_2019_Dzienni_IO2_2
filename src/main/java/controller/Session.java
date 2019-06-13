4b0e4ca8cec453e5e36b2ebd105b5d743cbad041
Karol Oleksy <karol.oleksy97@gmail.com>
2019-06-13 13:13:27 +0200

    Oblicznaie sesji wzrostowych spadkowych i bez zmian dla zadanego okresu czasu. Zastosowanie wzorca projektowego.

---
+++ src/main/java/controller/Session.java
@@ -0,0 +1,10 @@
+package controller;
+
+import model.Rate;
+
+import java.util.List;
+
+public interface Session {
+
+    public void countSession(List<Rate> rate);
+}
---
+++ src/main/java/controller/StaticSession.java
@@ -0,0 +1,22 @@
+package controller;
+
+import model.Rate;
+
+import java.util.List;
+
+public class StaticSession implements Session {
+
+    int staticSessionCounter = 0;
+
+    @Override public void countSession(List<Rate> rate) {
+
+        for (int i = 0; i < rate.size() - 2; i++) {
+            if (rate.get(i).getMid() == rate.get(i + 1).getMid())
+                staticSessionCounter++;
+        }
+    }
+
+    public int getStaticSessionCounter() {
+        return staticSessionCounter;
+    }
+}
