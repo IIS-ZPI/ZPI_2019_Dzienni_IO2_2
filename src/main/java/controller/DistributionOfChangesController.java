28f353930174bd402b716e57324d9c3a484e6f26
skydriver95 <34252075+skydriver95@users.noreply.github.com>
2019-06-30 18:58:05 +0200

    Refactored code

--- src/main/java/controller/DistributionOfChangesController.java
+++ src/main/java/controller/DistributionOfChangesController.java
@@ -77,22 +77,6 @@ public class DistributionOfChangesController {
 
         countSubstaction("kwartalny", currencyHistoryFirstQuarter, currencyHistorySecondQuarter);
 
-
-//
-//        CoefficientOfVariation coefficientOfVariation = new CoefficientOfVariation();
-//        Dominat dominat = new Dominat();
-//        Median median = new Median();
-//        StandardDeviation standardDeviaton = new StandardDeviation();
-//
-//        dominat.countStatisticalMeasure(currencyHistory.getRates());
-//        median.countStatisticalMeasure(currencyHistory.getRates());
-//        standardDeviaton.countStatisticalMeasure(currencyHistory.getRates());
-//        coefficientOfVariation.countStatisticalMeasure(standardDeviaton.getStandardDeviation(), standardDeviaton.getMean());
-//
-//        System.out.println("CoefficientOfVariation: " + coefficientOfVariation.getCoefficientOfVariation() + "%");
-//        System.out.println("Dominat: " + dominat.getDominate());
-//        System.out.println("Median: " + median.getMedian());
-//        System.out.println("StandardDeviation: " + standardDeviaton.getStandardDeviation());
     }
 
     public void countSubstaction(String option, CurrencyHistory currancyControllerFirst, CurrencyHistory currencyHistorySecond) {
--- src/main/java/controller/MeasureController.java
+++ src/main/java/controller/MeasureController.java
@@ -3,74 +3,13 @@ package controller;
 import controller.measure.*;
 import model.*;
 
-import java.text.SimpleDateFormat;
-import java.util.InputMismatchException;
-import java.util.Scanner;
 
-public class MeasureController {
+public class MeasureController extends Reader{
 
-    private Scanner scanner = new Scanner(System.in);
-    private CurrencyList currencyList;
-
-    int rangeSelected = 0;
-    boolean correctOption = false;
 
     public void countMeasure() throws Exception {
 
-        String pattern = "yyyy-MM-dd";
-        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
-
-        CurrencyCalendar cC = new CurrencyCalendar();
-
-        System.out.println("Enter session date range:");
-        System.out.println("1 - week");
-        System.out.println("2 - 2 weeks");
-        System.out.println("3 - month");
-        System.out.println("4 - 3 months (quarter)");
-        System.out.println("5 - 6 months");
-        System.out.println("6 - year");
-        System.out.print("> ");
-
-        // wczytywanie opcji
-
-        while(!correctOption){
-            try {
-                rangeSelected = scanner.nextInt();
-            }catch (InputMismatchException e){
-                //System.out.println("Wrong choice. Try again!");
-                scanner.next();
-            }
-            if(rangeSelected >0 && rangeSelected < 7)
-                correctOption = true;
-            else
-                System.out.println("Wrong choice. Try again!");
-        }
-        cC.setDateRange(rangeSelected);
-
-        // pobieranie walut z NBP
-        CurrancyController currancyController1 = new CurrancyController("gbp", "2019-01-01", "2019-01-08");
-        currencyList = currancyController1.getCurrencyList();
-
-        System.out.println("\nAvailable value codes: ");
-        for (Table t : currencyList.getTableRates()) {
-            System.out.print(t.getCode() + " ");
-        }
-
-        String code = readCode();
-
-        // format daty
-        String startDateFormat = sdf.format(cC.getStartDate());
-        String endDateFormat = sdf.format(cC.getEndDate());
-        System.out.println(startDateFormat + " - " + endDateFormat);
-
-        // pobieranie dat i wartości waluty z NBP
-        CurrancyController currancyController = new CurrancyController(code, startDateFormat, endDateFormat);
-        CurrencyHistory currencyHistory = currancyController.getHistory();
-        System.out.println(currencyHistory.getCurrency());
-
-        for (Rate r : currencyHistory.getRates()) {
-            System.out.println(r.getEffectiveDate() + "\t" + r.getMid());
-        }
+        CurrencyHistory currencyHistory = printOptionsAndRead();
 
         CoefficientOfVariation coefficientOfVariation = new CoefficientOfVariation();
         Dominat dominat = new Dominat();
@@ -87,43 +26,10 @@ public class MeasureController {
         System.out.println("Median: " + median.getMedian());
         System.out.println("StandardDeviation: " + standardDeviaton.getStandardDeviation());
 
-//        GrowthSession growthSession = new GrowthSession();
-//        StaticSession staticSession = new StaticSession();
-//        DownwardSession downwardSession = new DownwardSession();
-//
-//        growthSession.countSession(currencyHistory.getRates());
-//        staticSession.countSession(currencyHistory.getRates());
-//        downwardSession.countSession(currencyHistory.getRates());
-
-//        System.out.println("Liczba sesji wzrostowych: " + growthSession.getGrowthSessionCounter());
-//        System.out.println("Liczba sesji stałych: " + staticSession.getStaticSessionCounter());
-//        System.out.println("Liczba sesji spadkowych: " + downwardSession.getDownwardSessionCounter());
     }
 
 
-    private String readCode(){
-        String input = null;
-        System.out.print("\nEnter value code: ");
-        input = scanner.next();
-        System.out.println(input);
 
-
-        while(!containKey(input.toUpperCase())){
-            System.out.print("Incorrect currency. Try again\nEnter value code: ");
-            input = scanner.next();
-        }
-        return input;
-    }
-
-
-    private boolean containKey(String key){
-        for(Table t : currencyList.getTableRates()){
-            if(key.equals(t.getCode())){
-                return true;
-            }
-        }
-        return false;
-    }
 
 
 }
